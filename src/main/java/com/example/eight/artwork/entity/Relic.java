5eb7065509eff73537c6c652d8c2742cf944089e
yeni-choi <yewonchoi@ewhain.net>
2023-08-13 15:33:56 +0900

    :sparkles: [FEAT] 작품 Relic 엔티티 필드 추가

--- src/main/java/com/example/eight/artwork/entity/Relic.java
+++ src/main/java/com/example/eight/artwork/entity/Relic.java
@@ -1,10 +1,8 @@
 package com.example.eight.artwork.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.data.annotation.Id;
 
 import java.util.Optional;
@@ -19,4 +17,30 @@ import java.util.stream.Stream;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Relic {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "api_id")
+    private Long apiId;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @NotNull
+    @Column(name = "badge_image", nullable = false)
+    private String badgeImage;
+
+    @Column(name = "location")
+    private String location;
+
+    @NotNull
+    @Column(name = "part_num", nullable = false, columnDefinition = "TINYINT UNSIGNED DEFAULT 0")
+    private Integer partNum;
 }
