4743722ee32b3dd8974bf4420144af9bb776f658
jina kong <kongjn226@gmail.com>
2023-08-14 14:30:25 +0900

    :sparkles: [FEAT] User Entity 생성

---
+++ src/main/java/com/example/eight/user/entity/User.java
@@ -0,0 +1,41 @@
+package com.example.eight.user.entity;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String nickname;
+
+    @Column(nullable = false, length = 30)
+    private String email;
+
+    @Column(nullable = false, columnDefinition = "DECIMAL(10.1) default 0.0 ")
+    private double exp;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
+
