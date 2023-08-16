d5788ed7c6c28c24a655b9f78efcd860e8587e25
yeni-choi <yewonchoi@ewhain.net>
2023-08-16 14:23:04 +0900

    :sparkles: [FEAT] controller 에 요소 인식 기능 추가 #5

--- src/main/java/com/example/eight/artwork/controller/ArtworkController.java
+++ src/main/java/com/example/eight/artwork/controller/ArtworkController.java
@@ -1,8 +1,13 @@
 package com.example.eight.artwork.controller;
 
+import com.example.eight.artwork.dto.DetectionRequestDto;
+import com.example.eight.artwork.dto.DetectionResponseDto;
 import com.example.eight.artwork.service.ArtworkService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/app/artwork")
 public class ArtworkController{
     private final ArtworkService artworkService;
+
+    //요소 인식
+    @PostMapping("/detection")
+    public ResponseEntity<DetectionResponseDto> performDetection(@RequestBody DetectionRequestDto requestDto) {
+        DetectionResponseDto responseDto = artworkService.performDetection(requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
 
 }
