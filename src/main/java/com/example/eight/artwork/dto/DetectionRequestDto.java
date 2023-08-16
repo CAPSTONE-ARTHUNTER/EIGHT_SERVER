d30c00e6513b3fb6cfd0db5192be923acfbd265b
yeni-choi <yewonchoi@ewhain.net>
2023-08-16 13:14:47 +0900

    :truck: [RENAME] response dto 파일명 수정 #5

--- src/main/java/com/example/eight/artwork/dto/DetectionRequestDto.java
+++ src/main/java/com/example/eight/artwork/dto/DetectionRequestDto.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class DetectionRequest {
+public class DetectionRequestDto {
     private double time;
     private ImageInfo image;
     private List<Prediction> predictions;
