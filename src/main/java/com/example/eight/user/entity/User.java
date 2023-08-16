5849a4ad8a5801063cd4a3d912005ee911d7246c
jina kong <kongjn226@gmail.com>
2023-08-16 23:55:43 +0900

    :construction: 임시로 null 값 허용

--- src/main/java/com/example/eight/user/entity/User.java
+++ src/main/java/com/example/eight/user/entity/User.java
@@ -16,12 +16,13 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    public Long id;
 
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false, length = 30)
+    //@Column(nullable = false, length = 30)
+    @Column(length = 30)
     private String nickname;
 
     @Column(nullable = false, length = 30)
@@ -30,13 +31,16 @@ public class User {
     @Column
     private String picture;
 
-    @Column(nullable = false, columnDefinition = "DECIMAL(10.1) default 0.0 ")
+    //@Column(nullable = false, columnDefinition = "DECIMAL(10.1) default 0.0 ")
+    @Column(columnDefinition = "DECIMAL(10.1) default 0.0 ")
     private double exp;
 
-    @Column(name = "created_at", nullable = false)
+    //@Column(name = "created_at", nullable = false)
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at", nullable = false)
+    //@Column(name = "updated_at", nullable = false)
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
     @Enumerated(EnumType.STRING)
@@ -44,6 +48,7 @@ public class User {
     private Role role;
 
 
+    // 생성자
     @Builder
     public User(String name, String nickname, String email, double exp, String picture,
                 LocalDateTime createdAt, LocalDateTime updatedAt,
