2b32795806cea475ec7d60fd7835321f47a85404
yeni-choi <yewonchoi@ewhain.net>
2023-08-17 12:03:03 +0900

    :bug: [FIX] 요소(element) 조회하는 커스텀 쿼리 구현 #5

--- src/main/java/com/example/eight/artwork/repository/ArtworkRepository.java
+++ src/main/java/com/example/eight/artwork/repository/ArtworkRepository.java
@@ -2,11 +2,17 @@ package com.example.eight.artwork.repository;
 
 import com.example.eight.artwork.entity.Element;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface ArtworkRepository extends JpaRepository<Element, Long> {
-    //Element findElementByClassName(String elementClassName);
+    @Query("SELECT e FROM Element e WHERE e.name = :elementClassName")
+    Element findElementByClassName(String elementClassName);
 
-    //SolvedElement saveSolvedElement(SolvedElement solvedElement);
+    @Query("INSERT INTO SolvedElement(element, isSolved, solvedAt) VALUES (:element, :isSolved, :solvedAt)")
+    void saveSolvedElement(@Param("element") Element element, @Param("isSolved") boolean isSolved, @Param("solvedAt") LocalDateTime solvedAt);
 }
