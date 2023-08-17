b467d5f3848cf751165050ab289f95750c9de30c
yeni-choi <yewonchoi@ewhain.net>
2023-08-17 12:07:17 +0900

    :sparkles: [FEAT] request로부터 DetectionData 생성하는 메서드 구현 #5

--- src/main/java/com/example/eight/artwork/dto/DetectionResponseDto.java
+++ src/main/java/com/example/eight/artwork/dto/DetectionResponseDto.java
@@ -16,5 +16,12 @@ public class DetectionResponseDto {
     public static class DetectionData {
         private String image;
         private String name;
+
+        public static DetectionData createFromRequest(DetectionRequestDto requestDto) {
+            return DetectionData.builder()
+                    .image(requestDto.getImage().toString())  // 요소 이미지 URL
+                    .name(requestDto.getPredictions().get(0).getElementClassName())  // 요소 이름(class)
+                    .build();
+        }
     }
 }
