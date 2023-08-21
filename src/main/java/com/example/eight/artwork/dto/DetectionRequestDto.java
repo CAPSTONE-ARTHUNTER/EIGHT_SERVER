72a39de084fb203593887ad6a15159c0fc968edd
yeni-choi <yewonchoi@ewhain.net>
2023-08-21 10:17:51 +0900

    :recycle: [REFACTOR] 로직 변경에 따른 DTO 수정 #5

--- src/main/java/com/example/eight/artwork/dto/DetectionRequestDto.java
+++ src/main/java/com/example/eight/artwork/dto/DetectionRequestDto.java
@@ -10,32 +10,9 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class DetectionRequestDto {
-    private double time;
-    private ImageInfo image;
-    private List<Prediction> predictions;
+    private String name;
 
     public DetectionResponseDto.DetectionData getData() {
         return DetectionResponseDto.DetectionData.createFromRequest(this);
-    }
-
-    @Getter
-    @Builder
-    public static class ImageInfo {
-        private int width;
-        private int height;
-    }
-
-    @Getter
-    @Builder
-    public static class Prediction {
-        private double x;
-        private double y;
-        private double width;
-        private double height;
-        private double confidence;
-
-        //class는 예약어로 변수명 변경
-        @JsonProperty("class")
-        private String elementClassName;
     }
 }
