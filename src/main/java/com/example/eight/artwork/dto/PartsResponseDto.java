4735dc8b8dd3dd3a2b138a12f845a6adec1641a4
yeni-choi <yewonchoi@ewhain.net>
2023-08-21 16:50:39 +0900

    :sparkles: [FEAT] 작품 소제목 조회 DTO 생성 #8

---
+++ src/main/java/com/example/eight/artwork/dto/PartsResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.eight.artwork.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PartsResponseDto {
+    private String relicName;
+    private String relicImage;
+    private List<PartInfoDto> partInfos;
+    private int totalPartCount;
+}
