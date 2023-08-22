67548552ebef3934caa8cf1d310e60647989dc21
yeni-choi <yewonchoi@ewhain.net>
2023-08-22 15:00:07 +0900

    :truck: [RENAME] artworkrepository 파일명 변경 #8

--- src/main/java/com/example/eight/artwork/repository/ElementRepository.java
+++ src/main/java/com/example/eight/artwork/repository/ElementRepository.java
@@ -2,14 +2,10 @@ package com.example.eight.artwork.repository;
 
 import com.example.eight.artwork.entity.Element;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
 @Repository
-public interface ArtworkRepository extends JpaRepository<Element, Long> {
+public interface ElementRepository extends JpaRepository<Element, Long> {
     Element findByName(String name);
 
     @Override
