e119b89e2d5f8ef801248ea16ac90e6b61e7f891
jina kong <kongjn226@gmail.com>
2023-08-24 20:40:38 +0900

    :recycle: [REFACTOR] 소셜 로그인 실패시 response 추가

--- src/main/java/com/example/eight/global/oauth2/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/com/example/eight/global/oauth2/handler/OAuth2LoginFailureHandler.java
@@ -3,17 +3,21 @@ package com.example.eight.global.oauth2.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
     
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        // TODO: OAuth 로그인 실패시 로직
+        // OAuth 로그인 실패시 로직
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        log.info("소셜 로그인 실패 : {}", exception.getMessage());
     }
 }
