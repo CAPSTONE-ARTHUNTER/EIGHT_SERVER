2bc4a0ed57772387873185a50c9a13e2ffca4d1e
jina kong <kongjn226@gmail.com>
2023-08-24 20:09:42 +0900

    :sparkles: [FEAT] JWT 서비스 클래스 생성

---
+++ src/main/java/com/example/eight/global/jwt/JwtService.java
@@ -0,0 +1,12 @@
+package com.example.eight.global.jwt;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Getter
+public class JwtService {
+
+}
