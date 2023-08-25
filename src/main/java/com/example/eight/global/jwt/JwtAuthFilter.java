d892858f8fe7b2d75e9fdd2e9e7639e808e9fafe
jina kong <kongjn226@gmail.com>
2023-08-26 03:22:15 +0900

    :sparkles: [FEAT] Request 토큰 검증을 위한 JwtAuthFilter 생성

---
+++ src/main/java/com/example/eight/global/jwt/JwtAuthFilter.java
@@ -0,0 +1,26 @@
+package com.example.eight.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+
+/*
+모든 request의 토큰 검사
+ */
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
+
