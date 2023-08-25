67c98908e95723f03f2d528560b068142a42ba50
jina kong <kongjn226@gmail.com>
2023-08-26 01:45:17 +0900

    :sparkles: [FEAT] Request 헤더에서 Token 추출하는 메소드 추가

--- src/main/java/com/example/eight/global/jwt/JwtService.java
+++ src/main/java/com/example/eight/global/jwt/JwtService.java
@@ -3,6 +3,7 @@ package com.example.eight.global.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.example.eight.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -44,7 +46,6 @@ public class JwtService {
      */
     public String createAccessToken(String email) {
         Date now = new Date();
-        log.info("now: "+String.valueOf(now));
 
         return JWT.create()
                 .withSubject("AccessToken") // Subject를 AccessToken으로 지정
@@ -76,13 +77,33 @@ public class JwtService {
     }
 
     /*
- AccessToken과 RefreshToken을 헤더로 보내기
- */
+    AccessToken과 RefreshToken을 헤더로 보내기
+    */
     public void sendTokens(HttpServletResponse response, String accessToken, String refreshToken) {
         response.setStatus(HttpServletResponse.SC_OK);
 
         response.setHeader(accessHeader, accessToken);      // Acess Token을 헤더에 싣기
         response.setHeader(refreshHeader, refreshToken);    // Refresh Token을 헤더에 싣기
-        log.info("Access Token 및 Refresh Token 헤더로 설정 완료");
+        log.info("Access Token 및 Refresh Token 헤더로 보내기 성공");
+    }
+
+    /*
+    request 헤더에서 Token 추출
+     */
+    public Optional<String> getToken(HttpServletRequest request, String tokenType) {
+        String header;
+        switch (tokenType) {    // accessToken인지 refreshToken인지 구분
+            case "accessToken" -> header = accessHeader;
+            case "refreshToken" -> header = refreshHeader;
+            default -> {
+                log.info("유효하지 않습니다.'accessToken' 혹은 'refreshToken'을 입력하세요");
+                return Optional.empty();
+            }
+        }
+
+        // 해당 토큰 추출해서 리턴
+        return Optional.ofNullable(request.getHeader(header))
+                .filter(token -> token.startsWith("Bearer "))
+                .map(token -> token.replace("Bearer ", ""));    // 헤더에서 "Bearer" 삭제해서 토큰만 가져오기
     }
 }
