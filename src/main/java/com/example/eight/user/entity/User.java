03f53525dee964f8b7c00ebec806cb6f00d79ab1
jina kong <kongjn226@gmail.com>
2023-08-29 01:15:19 +0900

    :label: [REFACTOR] 유저의 nickname 필드를 name으로 변경 #1

--- src/main/java/com/example/eight/user/entity/User.java
+++ src/main/java/com/example/eight/user/entity/User.java
@@ -22,12 +22,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
-    //@Column(nullable = false)
+    @Column(nullable = false)
     private String name;
-
-    //@Column(nullable = false, length = 30)
-    @Column(length = 30)
-    private String nickname;
 
     @Column(nullable = false, length = 30)
     private String email;
@@ -58,11 +54,10 @@ public class User {
 
     // 생성자
     @Builder
-    public User(String name, String nickname, String email, double exp, String picture,
+    public User(String name, String email, double exp, String picture,
                 LocalDateTime createdAt, LocalDateTime updatedAt,
                 Role role, SocialType socialType){
         this.name = name;
-        this.nickname = nickname;
         this.email = email;
         this.exp = exp;
         this.picture = picture;
