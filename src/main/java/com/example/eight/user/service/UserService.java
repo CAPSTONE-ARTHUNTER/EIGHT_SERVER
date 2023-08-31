232b6ac03c4511eee70c210e7dbca0a7967d78bc
jina kong <kongjn226@gmail.com>
2023-08-31 22:42:12 +0900

    ✨ [FEAT] 현재 로그인한 유저 정보 가져오는 기능 추가 #15

--- src/main/java/com/example/eight/user/service/UserService.java
+++ src/main/java/com/example/eight/user/service/UserService.java
@@ -4,19 +4,32 @@ import com.example.eight.user.entity.User;
 import com.example.eight.user.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
-//@Transactional
 @Service
 public class UserService {
-    //추가
     private final UserRepository userRepository;
 
     @Autowired
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
+    }
+
+    // 현재 로그인한 Authentication 가져오기
+    public User getAuthentication(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails principal = (UserDetails) authentication.getPrincipal();
+        String userEmail = principal.getUsername();
+
+        log.info("Authentication에서 가져온 유저 이메일: {}", userEmail);
+
+        // 로그인한 User 객체 반환
+        return userRepository.findByEmail(userEmail).orElse(null);
     }
 
     @Transactional
