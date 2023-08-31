7be4ff0d888942b4208cbdee1bcf95931036f1de
jina kong <kongjn226@gmail.com>
2023-08-31 22:48:02 +0900

    ✨ [FEAT] 로그인한 유저의 프로필 조회하는 기능 추가 #15

--- src/main/java/com/example/eight/user/service/UserService.java
+++ src/main/java/com/example/eight/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.eight.user.service;
 
+import com.example.eight.user.dto.UserProfileDto;
 import com.example.eight.user.entity.User;
 import com.example.eight.user.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -40,5 +41,22 @@ public class UserService {
             user.updateRefreshToken(refreshToken);
             userRepository.save(user); // 변경 사항을 데이터베이스에 저장
         }
+    }
+
+    public UserProfileDto getUserProfile() {
+
+        User user = getAuthentication();    // 현재 로그인한 유저 정보
+
+        return UserProfileDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .picture(user.getPicture())
+                .exp(user.getExp())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .role(user.getRole())
+                .socialType(user.getSocialType())
+                .build();
     }
 }
