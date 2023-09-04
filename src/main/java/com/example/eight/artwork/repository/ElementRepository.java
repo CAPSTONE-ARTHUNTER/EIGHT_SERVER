ab595e92b6b4ddaa97b1ae1a826ad57cd6c6fe39
jina kong <kongjn226@gmail.com>
2023-09-05 02:30:18 +0900

    ✨ [FEAT] part에 속하는 elements 리스트 조회 메소드 추가 #18

--- src/main/java/com/example/eight/artwork/repository/ElementRepository.java
+++ src/main/java/com/example/eight/artwork/repository/ElementRepository.java
@@ -1,12 +1,18 @@
 package com.example.eight.artwork.repository;
 
 import com.example.eight.artwork.entity.Element;
+import com.example.eight.artwork.entity.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ElementRepository extends JpaRepository<Element, Long> {
     Element findByName(String name);
+
+    // 부분에 속하는 element 리스트 조회
+    List<Element> findByPart(Part part);    
 
     @Override
     <S extends Element> S save(S entity);
