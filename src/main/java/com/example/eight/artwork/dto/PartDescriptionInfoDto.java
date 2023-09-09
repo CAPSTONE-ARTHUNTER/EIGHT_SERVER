62666d4683717c88a78bfc835066d8a893af563a
jina kong <kongjn226@gmail.com>
2023-09-09 22:28:57 +0900

    ✨ [FEAT] 부분해설 조회 DTO 생성 #20

---
+++ src/main/java/com/example/eight/artwork/dto/PartDescriptionInfoDto.java
@@ -0,0 +1,16 @@
+package com.example.eight.artwork.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PartDescriptionInfoDto {
+    private String name;
+    private String description;
+    private boolean isSolved;
+}
