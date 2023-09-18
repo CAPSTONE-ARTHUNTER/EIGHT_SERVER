68f00a5e771d191a8f2f7ae89e02243aafd5c08e
jina kong <kongjn226@gmail.com>
2023-09-18 21:14:29 +0900

    :fire: [UPDATE] Part 테이블에서 isSolved, solvedElementNum 삭제 #1

--- src/main/java/com/example/eight/artwork/entity/SolvedPart.java
+++ src/main/java/com/example/eight/artwork/entity/SolvedPart.java
@@ -23,12 +23,6 @@ public class SolvedPart implements Serializable {
     @JoinColumn(name = "part_id", nullable = false)
     private Part part;
 
-    @Column(name = "is_solved", nullable = false)
-    private boolean isSolved;
-
-    @Column(name = "solved_element_num", nullable = false)
-    private int solvedElementNum;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
@@ -37,10 +31,8 @@ public class SolvedPart implements Serializable {
     private LocalDateTime solvedAt;
 
     @Builder
-    public SolvedPart(Part part, boolean isSolved, int solvedElementNum, User user, LocalDateTime solvedAt) {
+    public SolvedPart(Part part, User user, LocalDateTime solvedAt) {
         this.part = part;
-        this.isSolved = isSolved;
-        this.solvedElementNum = solvedElementNum;
         this.user = user;
         this.solvedAt = solvedAt;
     }
--- src/main/java/com/example/eight/artwork/repository/SolvedPartRepository.java
+++ src/main/java/com/example/eight/artwork/repository/SolvedPartRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SolvedPartRepository extends JpaRepository<SolvedPart, Long> {
-    int countByPartAndIsSolved(Part part, boolean isSolved);
 
     boolean existsByUserIdAndPartId(Long userId, Long partId);
 }
