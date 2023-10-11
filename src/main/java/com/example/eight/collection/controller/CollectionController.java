d36e73c2cf778608c423643079fe74e7b35103b5
jina kong <kongjn226@gmail.com>
2023-10-12 00:06:32 +0900

    :package: [FEAT] 도감(collection) Controller, Service 클래스 생성 #55

---
+++ src/main/java/com/example/eight/collection/controller/CollectionController.java
@@ -0,0 +1,13 @@
+package com.example.eight.collection.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/app/collection")
+public class CollectionController {
+
+    
+}
