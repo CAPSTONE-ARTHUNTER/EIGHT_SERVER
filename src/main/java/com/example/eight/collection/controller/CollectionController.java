648307306c643b540ce2de0b6a9b99faf5ab78a1
jina kong <kongjn226@gmail.com>
2023-10-12 00:24:05 +0900

    :sparkles: [FEAT] 도감 overview 조회 컨트롤러 매핑 #55

--- src/main/java/com/example/eight/collection/controller/CollectionController.java
+++ src/main/java/com/example/eight/collection/controller/CollectionController.java
@@ -1,7 +1,12 @@
 package com.example.eight.collection.controller;
 
+import com.example.eight.collection.dto.OverviewResponseDto;
+import com.example.eight.collection.service.CollectionService;
+import com.example.eight.global.ResponseDto;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +15,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CollectionController {
 
     
+    private final CollectionService collectionService;
+
+    // 수집 오버뷰 조회 API
+    @GetMapping("/overview")
+    public ResponseEntity<ResponseDto> getCollectionOverview(){
+        OverviewResponseDto overviewResponseDto = collectionService.getOverview();
+
+        ResponseDto responseDto = ResponseDto.builder()
+                .status("success")
+                .message("Get Overview successful")
+                .data(overviewResponseDto)
+                .build();
+
+        return ResponseEntity.ok(responseDto);
+
+    }
+
 }
