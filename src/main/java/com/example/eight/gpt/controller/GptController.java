8caab545aba57e76375d427df8713e24bf903f89
yeni-choi <yewonchoi@ewhain.net>
2023-11-01 22:32:56 +0900

    :sparkles: [FEAT] GPT controller 생성 #66

--- src/main/java/com/example/eight/gpt/controller/GptController.java
+++ src/main/java/com/example/eight/gpt/controller/GptController.java
@@ -1,10 +1,9 @@
 package com.example.eight.gpt.controller;
 
-import com.example.eight.global.ResponseDto;
-import com.example.eight.artwork.dto.*;
-import com.example.eight.artwork.service.ArtworkService;
+import com.example.eight.gpt.service.*;
+import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import com.example.eight.gpt.dto.GptResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,11 +11,16 @@ import java.util.List;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/app/artwork")
+@RequestMapping("/app/gpt")
 public class GptController {
-    private final GptService GptService;
+    private final ChatService chatService;
+    private final ChatgptService chatgptService;
+
     @PostMapping("/elementsinfo")
-    public GptResponseDto sendQuestion(@RequestBody QuestionRequestDto requestDto) {
-        return chatGptService.askQuestion(requestDto);
+    public String test(@RequestBody String question){
+        String answer = chatService.getChatResponse(question);
+        // JSON 형식의 응답 문자열 생성
+        String Response = "{\"answer\":\"" + answer + "\"}";
+        return Response;
     }
 }
