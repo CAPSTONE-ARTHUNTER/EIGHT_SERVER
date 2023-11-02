7c54426605a03dff7035247fcf77c6516b8619d0
yeni-choi <yewonchoi@ewhain.net>
2023-11-02 13:25:35 +0900

    :sparkles: [UPDATE] GPT controller 메서드 수정 #66

--- src/main/java/com/example/eight/gpt/controller/GptController.java
+++ src/main/java/com/example/eight/gpt/controller/GptController.java
@@ -1,25 +1,21 @@
 package com.example.eight.gpt.controller;
 
 import com.example.eight.gpt.service.*;
-import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.AllArgsConstructor;
 import com.example.eight.gpt.dto.GptResponseDto;
+import com.example.eight.gpt.dto.GptRequestDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/app/gpt")
 public class GptController {
     private final ChatService chatService;
-    private final ChatgptService chatgptService;
 
     @PostMapping("/elementsinfo")
-    public ResponseEntity<GptResponseDto> test(@RequestBody String question) {
-        String answer = chatService.getChatResponse(question);
+    public ResponseEntity<GptResponseDto> getElementInfo(@RequestBody GptRequestDto requestDto) {
+        String answer = chatService.getElementInfo(requestDto.getName());
 
         GptResponseDto response = new GptResponseDto(answer);
         return ResponseEntity.ok(response);
