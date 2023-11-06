875ac16bf041342627305cb3d41793f9c92b7e13
yeni-choi <yewonchoi@ewhain.net>
2023-11-06 14:15:15 +0900

    :sparkles: [UPDATE] Gpt Response Dto 파일에 멤버 변수 추가 #71

--- src/main/java/com/example/eight/gpt/dto/GptResponseDto.java
+++ src/main/java/com/example/eight/gpt/dto/GptResponseDto.java
@@ -6,10 +6,34 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class GptResponseDto {
+    private Long relicId;
+    private String relicName;
+    private String ElementImage;
+    private String ElementName;
     private String ElementDescription;
 
-    public GptResponseDto(String ElementDescription) {
+    public GptResponseDto(Long relicId, String relicName, String ElementImage, String ElementName, String ElementDescription) {
+        this.relicId = relicId;
+        this.relicName = relicName;
+        this.ElementImage = ElementImage;
+        this.ElementName = ElementName;
         this.ElementDescription = ElementDescription;
+    }
+
+    public Long getRelicId() {
+        return relicId;
+    }
+
+    public String getRelicName() {
+        return relicName;
+    }
+
+    public String getElementImage() {
+        return ElementImage;
+    }
+
+    public String getElementName() {
+        return ElementName;
     }
 
     public String getElementDescription() {
